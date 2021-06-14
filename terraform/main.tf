51cd5694120026ddc02c8dd78c811125dc9f4c49
enescinar914 <84137696+enescinar914@users.noreply.github.com>
2021-06-14 23:08:16 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,7 @@
 provider "aws" {
-  alias  = "central"
   region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" {
   bucket = "sm703bucket"
-  acl    = "private"
-
-  tags = {
-    Name        = "sm703bucket"
-    Environment = "Main"
-  }
 }
