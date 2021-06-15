5fb50429d3bd0ee1264f0b59a15626154f406335
enescinar914 <84137696+enescinar914@users.noreply.github.com>
2021-06-15 09:50:24 +0300

    Add region for s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket_object" "object" {
   bucket = "sm703bucket"
+  region = "us-east-2"
   source = "."
 
   # The filemd5() function is available in Terraform 0.11.12 and later
